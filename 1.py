d1f13d24a90a623fd8f4d86ffefbdb8a739a0bb5
djordjemaric <djordjegmaric@gmail.com>
2022-11-01 00:59:44 +0100

    formatiranje

--- 1.py
+++ 1.py
@@ -9,8 +9,9 @@ options.headless = True
 options.add_argument("--window-size=1920,1200")
 
 driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
-driver.get("https://suf.purs.gov.rs/v/?vl=A1I3S0dZQ1hNUjdLR1lDWE1IgAAA5HsAAGCYPQYAAAAAAAABhC3dVCwAAABNv%2FFRFdxQNq0mixnUo%2BBRv94Ooh%2BNbslAK6NuNCdifzt78jfw%2FN68s6LnoFz5GC5eMPrbn5wNZ57HjDl9el%2Bh%2FJSFzK%2FEcYFiTYmBQ0QPH9O2%2FtamrVbDTSv1I1YZmDgNUkEJBlA8QBO%2BE3OhHdy6n4TIQG2Coa6uQQVxqgqW8LNaWnMGc8VrPtjt3aFBil45xIX686ghKbsOG5867eZOnehI2GHGAPoO0mdDxT70IK4TssJ0YzY8bCJB5TZHxEDU7XU5eJ0VD7GT2sWO6mOyZWDvsQAuwFMaVzY6qBSVcwkW%2FjUZaAbyFcisqqNlA1K6vcAVaDqe%2F52Aw3LL61X4gLuCVBirMAnO8ftZvFX09XCCsmAquVt45uG2e4RZC4ERPm6TKs35I6%2BHlFRu3jS7XtM2Ba%2FNL3St0TwNThNBMnSpHBsUjO34Nyf6SN%2B0OFYC9SkXevtoKQtsUDF5rNc8LTg414R4YS6QAvpm7bEVfhHkxzDqkDrdGgh%2F0LP1kbeadh7BiARuOIR23cyJ2yE%2FKy1WliI3mVpiQVP3jS2ZUzvCyS2JY0E%2B2qwHvxiMcAvKS5KFFCct50%2BpTPYuL1YKWDOSUwIixMA%2F7o8Mh0MhyTCLbgjlPkd1j0etYhhxJo6XLfOad9JfjU09BtA8IoOSl8ni6z3G%2FYknzWh8qcLQfk5uh3fvWR01zfUnzL7DAaI%3D")
+driver.get("https://suf.purs.gov.rs/v/?vl=A1RMVE5QTjlBVExUTlBOOUHrZQIAlGQCAAA+SQAAAAAAAAABhC5ycbgAAAAvmGh+K/QGa9fPmVrcBd1O1eBz7SK7CP2/ABrNczyklPqjt3gBczbUcl7nBO2OhHp4X5eZs534ljddNz1/5sDTZFSKQxCP9On/l81uY6TjBPE3D0EsXIw/T7C3eg8O8Y10E2/rS+NAKan5Xc047kQTT1rZnXP8Elk6t7QxBtAms9lrxeuVx89Q57+QRvgWAoD91DbUf0iPuhyaQtAPvArQsEwrWqRBRxgkYOjJ1vjRhxPcy1gL1M75Oh0wwUma4dMA9ZJe6WRtybhEQWyAMhXiiYlBvHYSVcSTaCK66vQCXi9HjUyxrjLuanuQJMwLuP9CLsBLXuaXYME3lR6wyTrkL/6MCOciGDscXAWUGkxMBAQs09PCW23u0dzNtdESleKPKEpkHeoYuRxqe9xLJ8X2uyQOIPzY4OVXUIkvc09EEQJZsD6r/KsrKMWT5LdjwTTzPZkJ5+TtSTp0cjevuCCKrkYmozh4Sjgv8V8qH1j5a8kvJfPQ0+tP5rxVK+YzqIerRRF0MeDLvaSA/1gfxoWRqBEUOE7fZ2cEC3034yU+ywhPj3ksZJRXptzsYZR0lB8tEpNcdNiEEpWO9qkwKpSk4LZdZCX5eatN1Z0G6vU7QALqOYP4JOuix/YnI0jW250bAeuHafRLb3hGCEKwUrINjKscPHaMB8LY7WK6Dhx3z1DadBUqSS047tNTxP16s5Q=")
 #print(driver.page_source)
+prodajno_mesto = driver.find_element(By.XPATH, "//*[contains(text(),'Име продајног места')]/../div[@class='col-md-12']") 
 iznos = driver.find_element(By.XPATH, "//*[contains(text(),'Укупан износ')]/../div[@class='col-md-12']")
-print(iznos.text)
+print(iznos.text + ' RSD potroseno u ' + prodajno_mesto.text)
 driver.quit()
